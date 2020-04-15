9269d48930e3a9ecab0638ea4f72b1b2425f51af
FrankZZ <frank@wamm.es>
2020-04-15 23:45:00 +0200

    fix: use HTTPS and speed-up

--- experiaboxv10a/device_tracker.py
+++ experiaboxv10a/device_tracker.py
@@ -4,6 +4,7 @@ Support for ExperiaBox V10A
 import base64
 import hashlib
 import logging
+import os
 import re
 from collections import namedtuple
 from datetime import datetime, timezone
@@ -15,7 +16,7 @@ import homeassistant.helpers.config_validation as cv
 import homeassistant.util.dt as dt_util
 from homeassistant.components.device_tracker import (
     DOMAIN, PLATFORM_SCHEMA, DeviceScanner)
-from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME
+from homeassistant.const import CONF_HOST, CONF_PASSWORD, CONF_USERNAME, CONF_SSL, CONF_SCAN_INTERVAL
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -41,10 +42,13 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
         """Initialize the scanner."""
         host = config[CONF_HOST]
         username, password = config[CONF_USERNAME], config[CONF_PASSWORD]
+        ssl = True
+
+        self.ca_cert_bundle = os.path.join(os.path.dirname(__file__), 'ca-bundle-kpn-pkio-g3-server.pem')
 
         self.parse_macs = re.compile('[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}:[0-9A-Fa-f]{2}')
 
-        self.host = host
+        self.base_url = 'http{}://{}'.format('s' if ssl else '', host)
 
         self.username = hashlib.sha512(hashlib.md5(username.encode('utf-8')).hexdigest().encode('utf-8')).hexdigest()
 
@@ -78,6 +82,7 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
         """Ensure the information from the ExperiaBox V10A is up to date.
         Return boolean if scanning successful.
         """
+
         _LOGGER.info("Loading devices...")
 
         # We need to store a cookie
@@ -87,8 +92,8 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
         ts = round(datetime.now(timezone.utc).timestamp() * 1000)
 
         # Retrieve login.htm first, to retrieve the "httoken" to do the actual POST /login.cgi
-        login_url_initial = 'http://{}/login.htm'.format(self.host)
-        page_initial = session.get(login_url_initial)
+        login_url_initial = '{}/login.htm'.format(self.base_url)
+        page_initial = session.get(login_url_initial, timeout = 10, verify = self.ca_cert_bundle)
 
         # Security by obfuscation
         # The token is "hidden" as a base64 string, and is in the page source as a img with base64 data
@@ -105,9 +110,9 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
             "pws": self.password
         }
 
-        login_url = 'http://{}/login.cgi'.format(self.host)
+        login_url = '{}/login.cgi'.format(self.base_url)
         # this request will reply with a 302 Found, we don't want to be redirected just yet in order to do error handling
-        start_page = session.post(login_url, allow_redirects = False, data = login_payload, timeout=5, headers = {'referer': 'http://{}/login.htm'.format(self.host)})
+        start_page = session.post(login_url, allow_redirects = False, data = login_payload, timeout = 10, verify = self.ca_cert_bundle, headers = {'referer': '{}/login.htm'.format(self.base_url)})
 
         _LOGGER.debug('login.cgi start_page')
         _LOGGER.debug(start_page.status_code)
@@ -122,7 +127,7 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
             _LOGGER.error('Could not log in to the device. Got redirected to {}'.format(start_page.headers['Location']))
             return False
 
-        index_page = session.get('http://{}{}'.format(self.host, start_page.headers['Location']), timeout=5, headers = {'referer': 'http://{}/login.htm'.format(self.host)})
+        index_page = session.get('{}{}'.format(self.base_url, start_page.headers['Location']), timeout = 10, verify = self.ca_cert_bundle, headers = {'referer': '{}/login.htm'.format(self.base_url)})
 
         _LOGGER.debug('index.htm index_page')
         _LOGGER.debug(index_page.status_code)
@@ -131,18 +136,10 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
         index_httoken_search = re.search("yH5BAEAAAAALAAAAAABAAEAAAIBRAA7(.+)\" border=0>", index_page.text)
         index_authenticity_token = base64.b64decode(index_httoken_search.group(1)).decode('utf-8')
 
-        home_map_page = session.get('http://{}/home_map.htm?t={}'.format(self.host, ts), timeout=5, headers = {'referer': 'http://{}/index.htm'.format(self.host)})
-        _LOGGER.debug('home_map.htm home_map_page')
-        _LOGGER.debug(home_map_page.status_code)
-
-        # this is the token that needs to be used to call /cgi/cgi_clients.js
-        home_map_httoken_search = re.search("yH5BAEAAAAALAAAAAABAAEAAAIBRAA7(.+)\" border=0>", home_map_page.text)
-        home_map_authenticity_token = base64.b64decode(home_map_httoken_search.group(1)).decode('utf-8')
-
         result = False
         try:
-            data_url = 'http://{}/cgi/cgi_clients.js?_tn={}'.format(self.host, home_map_authenticity_token, ts, ts)
-            data_page = session.get(data_url, timeout=5, headers={'referer': 'http://{}/home_map.htm?t={}'.format(self.host, ts)})
+            data_url = '{}/cgi/cgi_clients.js?_tn={}'.format(self.base_url, index_authenticity_token, ts, ts)
+            data_page = session.get(data_url, timeout = 10, verify = self.ca_cert_bundle, headers = {'referer': '{}/index.htm?t={}'.format(self.base_url, ts)})
             _LOGGER.debug('cgi_clients.js data_page')
             _LOGGER.debug(data_page.status_code)
             # response is a javascript file with various information, for now we just want the online clients.
@@ -156,8 +153,8 @@ class ExperiaBoxV10ADeviceScanner(DeviceScanner):
             "httoken": index_authenticity_token
         }
 
-        logout_url = 'http://{}/logout.cgi'.format(self.host)
-        log_out_page = session.post(logout_url, data = logout_payload, timeout=5, headers = {'referer': 'http://{}/index.htm'.format(self.host)})
+        logout_url = '{}/logout.cgi'.format(self.base_url)
+        log_out_page = session.post(logout_url, data = logout_payload, timeout = 10, verify=False, headers = {'referer': '{}/index.htm'.format(self.base_url)})
 
         now = dt_util.now()
 
